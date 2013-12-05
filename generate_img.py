36517591ccef6418e0bb485cbd4398ebb9517dd4
Clement Skau <clementskau@gmail.com>
2013-12-05 20:02:19 +0900

    Changes generate_img.py to generate single-line samples in the train directory

--- generate_img.py
+++ generate_img.py
@@ -1,30 +1,42 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # coding: UTF-8
+
+
+from os import walk, path
 
 import PIL
 from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 
+import fontforge
 
-FONT_DIR='/usr/share/fonts/OTF/'
+
+DOCS_DIR='documents'
+FONT_DIR='/usr/share/fonts/'
+
+IMAGE_EXT='png'
 
 
-def draw_text(lang, font_name, font_file, txt, margin=10):
-  boxes = []
-
+def draw_text(lang, font_name, exp, font_file, txt, margin=10):
   font = ImageFont.truetype(FONT_DIR + font_file, 18)
+  ff = fontforge.open(FONT_DIR + font_file)
+
+  font_glyphs = {glyph.unicode: glyph for glyph in ff.glyphs()}
+
+  lines = txt.split('\n')
 
   image_width = 0
   image_height = 0
-  for line in txt.split('\n'):
+  for line in lines:
     width, height = font.getsize(line)
     width += ((len(line) - 1) * margin)
     image_width = max(width, image_width)
-    image_height += height
+    image_height += height + margin
 
   image_width += 2 * margin
-  image_height += 2 * margin
+  image_height += margin
+
 
   img = Image.new(
       "RGBA",
@@ -33,37 +45,65 @@ def draw_text(lang, font_name, font_file, txt, margin=10):
       )
   draw = ImageDraw.Draw(img)
 
+  boxes = []
   x = margin
   y = margin
-  for line in txt.split('\n'):
+  for line in lines:
+    line_height = 0
     for c in line:
       width, height = font.getsize(c)
-      draw.text(
-          (x, y),
-          c,
-          (0, 0, 0),
-          font=font,
-          )
-      boxes.append(
-          (c, x, y, (x + width), (y + height))
-          )
+      line_height = max(height, line_height)
+      # only draw and box character if the glyph is in the font
+      if ord(c) in font_glyphs:
+        draw.text(
+            (x, y),
+            c,
+            (0, 0, 0),
+            font=font,
+            )
+        boxes.append(
+            (c, x, y, (x + width), (y + height))
+            )
       x += width + margin
-    y += height + margin
+    x = margin
+    y += line_height + margin
 
-  img.save('{}.{}.exp0.tiff'.format(lang, font_name))
+  img.save('train/{}.{}.exp{}.{}'.format(lang, font_name, exp, IMAGE_EXT))
 
-  with open('{}.{}.exp0.box'.format(lang, font_name), 'w+') as box_file:
-    for c, x, y, x2, y2 in boxes:
-      box_file.write(
-          '{} {} {} {} {} 0\n'.format(c, x, y, x2, y2)
-          )
+  if boxes:
+    box_file_path = 'train/{}.{}.exp{}.box'.format(lang, font_name, exp)
+    with open(box_file_path, 'w+') as box_file:
+      for c, x, y, x2, y2 in boxes:
+        box_file.write(
+            '{} {} {} {} {} 0\n'.format(c.encode('utf-8'), x, y, x2, y2)
+            )
 
 
 if __name__ == '__main__':
-  boxes = draw_text(
-      'jpn',
-      'IPAGothic',
-      'ipag.ttf',
-      '野島は自分で云っている内に、なんだかわけがわからなくなった。',
-      )
-  'UmePlusPGothic'
+  fonts = [
+    ('OTF/ipag.ttf', 'IPAGothic'),
+    ('OTF/ipamp.ttf', 'IPAPMincho'),
+    ('TTF/togoshi-gothic.ttf', 'TogoshiGothic'),
+    ('TTF/togoshi-mincho.ttf', 'TogoshiMincho'),
+    ('TTF/togoshi-monago.ttf', 'TogoshiMonago'),
+    ('TTF/togoshi-mono.ttf', 'TogoshiMono'),
+    ('TTF/umeplus-gothic.ttf', 'UmeplusGothic'),
+    ('TTF/kiloji.ttf', 'Kiloji'),
+    # NOTE: the below seem to render wrong for some or all content
+    # ('TTF/dejima-mincho.ttf', 'DejimaMincho'),
+    # ('TTF/sazanami-gothic.ttf', 'SazanamiGothic'),
+    # ('TTF/sazanami-mincho.ttf', 'SazanamiMincho'),
+    # ('TTF/DroidSansJapanese.ttf', 'DroidSansJapanese'),
+  ]
+
+  i = 0
+  texts = []
+  for (dirpath, dirnames, filenames) in walk(DOCS_DIR):
+    for filename in filenames:
+      with open(path.join(dirpath, filename)) as text_file:
+        text_content = text_file.read().decode('utf-8')
+        for line in text_content.splitlines():
+          if line:
+            for font_file, font_name in fonts:
+              draw_text('jpn', font_name, i, font_file, line)
+            i += 1
